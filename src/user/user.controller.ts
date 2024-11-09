4046593cc6cbd86f42e190abea199e3745c2c2bd
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-11-09 17:44:25 +0200

    feat: added routes to the /user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, UpdatePasswordDto } from 'src/interface/interface';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() user: CreateUserDto) {
+    return this.userService.create(user);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() userUpdate: UpdatePasswordDto) {
+    return this.userService.update(id, userUpdate);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import {
+  CreateUserDto,
+  UpdatePasswordDto,
+  User,
+} from 'src/interface/interface';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private user: User[] = [];
+
+  findAll() {
+    return this.user;
+  }
+
+  findOne(id: string) {
+    const user = this.user.find((user) => user.id === id);
+
+    return user;
+  }
+
+  create(user: CreateUserDto) {
+    const newUser = {
+      id: uuidv4(),
+      version: 1,
+      ...user,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    this.user.push(newUser);
+
+    return newUser;
+  }
+
+  update(id: string, updatedUser: UpdatePasswordDto) {
+    this.user = this.user.map((user) => {
+      if (user.id === id && user.password === updatedUser.oldPassword) {
+        return {
+          ...user,
+          password: updatedUser.newPassword,
+          updatedAt: Date.now(),
+          version: user.version + 1,
+        };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: string) {
+    const removedUser = this.findOne(id);
+
+    this.user = this.user.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
+}
