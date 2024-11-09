2cdee7591cb44ff9f2266ef1f1885104966eabeb
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-11-09 20:29:00 +0200

    feat: track is passed tests

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   ParseUUIDPipe,
   Post,
@@ -39,6 +40,7 @@ export class TrackController {
   }
 
   @Delete(':id')
+  @HttpCode(204)
   delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.trackService.delete(id);
   }
