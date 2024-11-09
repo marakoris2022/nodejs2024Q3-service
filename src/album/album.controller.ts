002e781d874f10a5f11c82aff03cee88c26b5f9c
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-11-09 21:25:14 +0200

    feat: added album

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
+import { AlbumService } from './album.service';
+import { Album } from './album.dto';
 
 @Controller('album')
-export class AlbumController {}
+export class AlbumController {
+  constructor(private readonly albumService: AlbumService) {}
+
+  @Get()
+  getAllAlbums() {
+    return this.albumService.getAllAlbums();
+  }
+
+  @Get(':id')
+  getAlbumById(@Param('id') id: string) {
+    return this.albumService.getAlbumById(id);
+  }
+
+  @Post()
+  createAlbum(@Body() album: Album) {
+    return this.albumService.createAlbum(album);
+  }
+
+  @Put(':id')
+  updateAlbum(@Param('id') id: string, @Body() album: Album) {
+    return this.albumService.updateAlbum(id, album);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  deleteAlbum(@Param('id') id: string) {
+    this.albumService.deleteAlbum(id);
+  }
+}
